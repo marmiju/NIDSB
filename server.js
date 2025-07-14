d31321397d7143a0d818ad22eebbc11fbea47d55
marmiju <mar.miju.dev@gmail.com>
2025-07-15 01:49:31 +0600

    vercel configure

--- server.js
+++ server.js
@@ -1,10 +1,9 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import cors from 'cors';
-import db from './database/DB.js'; // Import the database connection
 import router from './routes/Routers.js'; // Import routes
 
-const app = express();
+export const app = express();
 
 app.use(cors());
 app.use(bodyParser.json());
@@ -13,6 +12,10 @@ const PORT = process.env.PORT || 5000;
 
 
 app.use('/api', router); // Mount the routes
+
+app.get('/', (req, res) => {
+    res.send('Welcome to the NIDSB API');
+});
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
